1a404424da173651d5e732b06d760333f3687c92
kaleabmarkos <kaleabgessese@gmail.com>
2025-03-29 14:24:27 -0500

    gettask

--- pkg/database/test.go
+++ pkg/database/test.go
@@ -1,8 +1,11 @@
 package database
 
-import ("database/sql"
-		"log"
-	)
+import (
+	"database/sql"
+	"log"
+
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
 
 func createTask(db *sql.DB, title, description, status string) int64{
 	query := `INSERT INTO task (title, description, status) VALUES (?,?,?)`
@@ -20,3 +23,28 @@ func createTask(db *sql.DB, title, description, status string) int64{
 	return lastInsertID
 
 }
+
+func getTask(db *sql.DB) ([]Task, error){
+	row, err := db.Query("SELECT id, title, description, status, created_at, updated_at FROM tasks")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	defer row.Close()
+
+	var tasks []Task
+
+	for row.Next(){
+		var task Task
+
+		if err:=row.Scan(&task.ID, &task.Title, &task.Description, &task.Status, &task.CreatedAt, &task.UpdatedAt); err!=nil{
+			log.Fatal(err)
+			return nil,err
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
